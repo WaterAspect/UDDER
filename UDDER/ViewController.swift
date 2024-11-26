daa409adabfa49ffd741bdc59cc570bf3fb755fb
Tolgahan <tolgahanguney71@gmail.com>
2024-11-26 18:43:29 +0300

    ıuoawydawud8awd

--- UDDER/ViewController.swift
+++ UDDER/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     let anan = "anan"
+    
+    override func willMove(toParent parent: UIViewController?) {
+        
+    }
 
 
 }
